bcdc4b2977d2d2404be1fbc6598c07967244000d
wulongmiao <42402040+wulongmiao@users.noreply.github.com>
2020-10-02 22:30:39 +0800

    update

--- js/backtop.js
+++ js/backtop.js
@@ -1,16 +1,16 @@
 /*返回顶部：jquery实现*/
 $(document).ready(function() {
-
+    $("#back-top").hide();
 
     // 淡入淡出效果
-        $((window).scroll(function() {
+    $(function() {
+        $(window).scroll(function() {
             if ($(this).scrollTop() > 100) {
                 $('#back-top').fadeIn();
             } else {
                 $('#back-top').fadeOut();
             }
-        })
-        );
+        });
 
     // 回到顶部
         $('#back-top>a').click(function() {
