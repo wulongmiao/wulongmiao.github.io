9ef5017cdc61e90b0bbc9db41a2985c12fa33b3a
wulongmiao <42402040+wulongmiao@users.noreply.github.com>
2020-10-02 23:42:26 +0800

    updte

---
+++ js/backdown.js
@@ -0,0 +1,23 @@
+/*返回底部：jquery实现*/
+$(document).ready(function() {
+    
+    $("#back-down").hide();
+    // 淡入淡出效果
+    $(function() {
+        $(window).scroll(function() {
+            if ($(this).scrollTop() > 100) {
+                $('#back-down').fadeIn();
+            } else {
+                $('#back-down').fadeOut();
+            }
+        });
+    );
+
+    // 回到底部
+        $('#back-down>a').click(function() {
+            $('body,html').animate({
+                scrollDown: 0
+            }, 500);
+            return false;
+        });
+});
