56f6a000a44ae96c6a1a31734e3d5e9f154be4fa
wulongmiao <42402040+wulongmiao@users.noreply.github.com>
2020-10-03 20:46:35 +0800

    update

--- js/backtop.js
+++ js/backtop.js
@@ -1,17 +1,4 @@
 /*返回顶部：jquery实现*/
-    
-    $("#back-top").hide();
-    // 淡入淡出效果
-    $(function() {
-        $(window).scroll(function() {
-            if ($(this).scrollTop() > 100) {
-                $('#back-top').fadeIn();
-            } else {
-                $('#back-top').fadeOut();
-            }
-        });
-    );
-
     // 回到顶部
         $('#back-top>a').click(function() {
             $('body,html').animate({
@@ -19,25 +6,11 @@
             }, 500);
             return false;
         });
-        
-/*返回底部：jquery实现*/
-
-$("#back-down").hide();
-// 淡入淡出效果
-$(function() {
-    $(window).scroll(function() {
-        if ($(this).scrollTop()>100) {
-            $('#back-down').fadeIn();
-        } else {
-            $('#back-down').fadeOut();
-        }
-    });
-);
 
 // 回到底部
     $('#back-down>a').click(function() {
         $('body,html').animate({
-            scrollTop:$(document).height()-$(window).height()
+            scrollTop:$(document).height()
         }, 500);
         return false;
     });
