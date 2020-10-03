0a817c1cb2701bfa4365f8351dd4c4a86673d9c8
wulongmiao <42402040+wulongmiao@users.noreply.github.com>
2020-10-03 15:50:53 +0800

    update

--- js/backtop.js
+++ js/backtop.js
@@ -19,3 +19,25 @@
             }, 500);
             return false;
         });
+        
+/*返回底部：jquery实现*/
+
+$("#back-down").hide();
+// 淡入淡出效果
+$(function() {
+    $(window).scroll(function() {
+        if ($(this).scrollTop()>100) {
+            $('#back-down').fadeIn();
+        } else {
+            $('#back-down').fadeOut();
+        }
+    });
+);
+
+// 回到底部
+    $('#back-down>a').click(function() {
+        $('body,html').animate({
+            scrollTop:document.body.scrollHeight-document.body.clientHeight
+        }, 500);
+        return false;
+    });
