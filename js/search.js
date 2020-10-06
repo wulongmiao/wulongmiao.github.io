86d443bfd51471c3baa42678446f0562d5d8f231
wulongmiao <42402040+wulongmiao@users.noreply.github.com>
2020-10-06 16:58:08 +0800

    udate

--- js/search.js
+++ js/search.js
@@ -16,6 +16,13 @@ var searchValue = '',
 var tmpDiv = document.createElement('div');
 tmpDiv.className = 'result-item';
 
+// 清空按钮点击函数
+searchClear.onclick = function(){
+    searchInput.value = '';
+    searchResults.style.display = 'none';
+    searchClear.style.display = 'none';
+}
+
 // ajax 的兼容写法
 var xhr = new XMLHttpRequest() || new ActiveXObject('Microsoft.XMLHTTP');
 xhr.onreadystatechange = function () {
@@ -37,30 +44,6 @@ xhr.onreadystatechange = function () {
     }
 }
 
-// 开始获取根目录下 feed.xml 文件内的数据
-xhr.open('get', '/feed.xml', true);
-xhr.send();
-
-searchBtn.onclick = searchConfirm;
-
-// 清空按钮点击函数
-searchClear.onclick = function(){
-    searchInput.value = '';
-    searchResults.style.display = 'none';
-    searchClear.style.display = 'none';
-}
-
-// 输入框内容变化后就开始匹配，可以不用点按钮
-// 经测试，onkeydown, onchange 等方法效果不太理想，
-// 存在输入延迟等问题，最后发现触发 input 事件最理想，
-// 并且可以处理中文输入法拼写的变化
-searchInput.oninput = function () {
-    setTimeout(searchConfirm, 0);
-}
-searchInput.onfocus = function () {
-    searchResults.style.display = 'block';
-}
-
 function searchConfirm() {
     if (searchInput.value == '') {
         searchResults.style.display = 'none';
@@ -79,6 +62,27 @@ function searchConfirm() {
         searchMatching(arrTitles, arrContents, searchValue);
     }
 }
+
+// 开始获取根目录下 feed.xml 文件内的数据
+xhr.open('get', '/feed.xml', true);
+xhr.send();
+
+searchBtn.onclick = searchConfirm;
+
+
+
+// 输入框内容变化后就开始匹配，可以不用点按钮
+// 经测试，onkeydown, onchange 等方法效果不太理想，
+// 存在输入延迟等问题，最后发现触发 input 事件最理想，
+// 并且可以处理中文输入法拼写的变化
+searchInput.oninput = function () {
+    setTimeout(searchConfirm, 0);
+}
+searchInput.onfocus = function () {
+    searchResults.style.display = 'block';
+}
+
+
 
 // 每次搜索完成后的初始化
 function searchInit() {
@@ -116,13 +120,13 @@ function searchMatching(arr1, arr2, input) {
 
     // 输出总共匹配到的数目
     var totalDiv = tmpDiv.cloneNode(true);
-    totalDiv.innerHTML = '总匹配：<b>' + indexItem.length + '</b> 项';
+    totalDiv.innerHTML = '输入匹配：<b>' + indexItem.length + '</b> 项';
     searchResults.appendChild(totalDiv);
 
     // 未匹配到内容的情况
     if (indexItem.length == 0) {
         var itemDiv = tmpDiv.cloneNode(true);
-        itemDiv.innerText = '未匹配到内容...';
+        itemDiv.innerText = '输入内容无匹配项...';
         searchResults.appendChild(itemDiv);
     }
 
@@ -136,11 +140,12 @@ function searchMatching(arr1, arr2, input) {
     }
 }
 
+//匹配内容打开方式
 function changeHref(href) {
 
-    // 在当前页面点开链接的情况
+    // 在当前页面打开链接
     location.href = href;
 
-    // 在新标签页面打开链接的代码，与上面二者只能取一个，自行决定
+    // 在新标签页面打开链接
     // window.open(href);
 }
